042f08e2eec2a888e25785db350cb7caaa51175c
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-12-29 18:24:21 -0300

    Initial commit 9

---
+++ Source/OpenWorldStarter/Public/OWSAPI.h
@@ -0,0 +1,17 @@
+// Copyright 2021 Sabre Dart Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "OWSAPI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDSTARTER_API UOWSAPI : public UObject
+{
+	GENERATED_BODY()
+	
+};
